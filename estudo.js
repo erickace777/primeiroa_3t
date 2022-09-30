2ae020d49c6b65c650e312daf61229498d4219af
erickace777 <108939789+erickace777@users.noreply.github.com>
2022-09-30 11:12:12 -0300

    Add files via upload

--- estudo.js
+++ estudo.js
@@ -1,1 +1,17 @@
+let tabuada = 2;
+function escreva(){
+let nome = "Erick";
 
+    document.write("Tabuada do " + tabuada + "<br>")
+    document.write(tabuada +" x 1 = "+(tabuada*1)+"<br>");
+    document.write(tabuada +" x 1 = "+(tabuada*2)+"<br>");
+    document.write(tabuada +" x 1 = "+(tabuada*3)+"<br>");
+    document.write(tabuada +" x 1 = "+(tabuada*4)+"<br>");
+    document.write(tabuada +" x 1 = "+(tabuada*5)+"<br>");
+    document.write(tabuada +" x 1 = "+(tabuada*6)+"<br>");
+    document.write(tabuada +" x 1 = "+(tabuada*7)+"<br>");
+    document.write(tabuada +" x 1 = "+(tabuada*8)+"<br>");
+    document.write(tabuada +" x 1 = "+(tabuada*9)+"<br>");
+    document.write(tabuada +" x 1 = "+(tabuada*10)+"<br>");
+    document.write("Feito por" + nome);
+}
