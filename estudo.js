5d74966cd99f1c393afea88b80d2d9c03cc91e49
erickace777 <108939789+erickace777@users.noreply.github.com>
2022-10-05 08:21:37 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -11,7 +11,22 @@ let nome = "Erick";
     document.write(tabuada +" x 1 = "+(tabuada*6)+"<br>");
     document.write(tabuada +" x 1 = "+(tabuada*7)+"<br>");
     document.write(tabuada +" x 1 = "+(tabuada*8)+"<br>");
-    document.write(tabuada +" x 1 = "+(tabuada*9)+"<br>");
+    tabuada*9)+"<br>"document.write(tabuada +" x 1 = "+();
     document.write(tabuada +" x 1 = "+(tabuada*10)+"<br>");
     document.write("Feito por" + nome);
+}
+
+function minhaTabuada(){
+    for(let i = 1; i <= 10; i++){
+      document.write("Tabuada do " + i + "<br>");
+     for(let j = 1; j <= 10; j++){
+     document.write(i + " x  " + j + " = "+ (j*i)+ "<br>");
+        }
+        document.write("<br>");
+    }
+}
+ 
+function quadrado(){
+    for(let i = 1; i <= 20; i++){
+        document.write("O quadrado de " + i + " é " + (i*i)+ "<br>");
 }
